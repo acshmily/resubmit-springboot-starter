795c3581a61d7fbf57bbb11bfe6babb9db8cb2b0
acshmily <npc@newbie.town>
2022-06-13 17:55:47 +0800

    -fixed upload bug

--- src/main/java/io/github/acshmily/resubmit/config/ReSubmitFilter.java
+++ src/main/java/io/github/acshmily/resubmit/config/ReSubmitFilter.java
@@ -1,10 +1,16 @@
 package io.github.acshmily.resubmit.config;
 
 import io.github.acshmily.resubmit.interceptor.CustomHttpServletRequestWrapper;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Wrapper the request, fixed get input stream only once
@@ -13,7 +19,12 @@ public class ReSubmitFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         if(request instanceof HttpServletRequest){
-            ServletRequest requestWrapper = new CustomHttpServletRequestWrapper((HttpServletRequest) request);
+            // check is file upload
+            HttpServletRequest servletRequest = (HttpServletRequest) request;
+            if(!StringUtils.isEmpty(servletRequest.getContentType()) && MediaType.MULTIPART_FORM_DATA_VALUE.equalsIgnoreCase(servletRequest.getContentType())){
+                servletRequest = new StandardServletMultipartResolver().resolveMultipart(servletRequest);
+            }
+            ServletRequest requestWrapper = new CustomHttpServletRequestWrapper(servletRequest);
             chain.doFilter(requestWrapper,response);
             return;
         }
